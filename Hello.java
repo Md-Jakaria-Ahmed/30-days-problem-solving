a9f4b7f400cddc1a170f7c8e59068a268fe8a276
Md Zakaria Ahmed <jakaria9404@gmail.com>
2022-05-12 08:17:28 +0600

    problem solving hacker rank 30 days

---
+++ Hello.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		String st;
+		Scanner sc = new Scanner(System.in);
+		st = sc.nextLine();
+		System.out.println("Hello, World.");
+		System.out.println(st);
+
+	}
+
+}
